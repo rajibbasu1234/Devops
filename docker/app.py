c7fa3670fb03ea9c88b516c57006a5490a5e75a2
Rajib Basu <rajibbasu123@Rajibs-MacBook-Pro.local>
2024-04-29 03:51:16 -0700

    First revision | Working code

---
+++ docker/app.py
@@ -0,0 +1,36 @@
+# Modify your application to retrieve database credentials securely from AWS
+# Secrets Manager. You'll need AWS credentials configured that have permission
+# to access Secrets Manager.
+
+import boto3
+from botocore.exceptions import ClientError
+
+def get_secret():
+    secret_name = "MyDatabaseSecrets"
+    region_name = "us-east-1"
+
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+    except ClientError as e:
+        raise e
+    else:
+        if 'SecretString' in get_secret_value_response:
+            secret = get_secret_value_response['SecretString']
+            return secret
+        else:
+            decoded_binary_secret = base64.b64decode(get_secret_value_response['SecretBinary'])
+            return decoded_binary_secret
+
+# Use the retrieved secrets for your database credentials
+import json
+secrets = json.loads(get_secret())
+DB_USER = secrets['username']
+DB_PASS = secrets['password']
